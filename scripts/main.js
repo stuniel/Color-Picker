ba61ce8be065c4d88105a063e651dfeab312994e
stuniel <stuniel@gmail.com>
2017-11-15 20:48:10 +0100

    Finished color and alpha picker functionality, input fields show hex and rgba values.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 var wrapper = document.querySelector(".color-picker-container");
-var pickerFlag = false;
 var huePickerFlag = false;
 
 var panel = {
@@ -14,13 +13,15 @@ var panel = {
   height:300,
   hue:[255,0,0],
   mouseY: 0,
-  mouseX: 0
+  mouseX: 0,
+  huePickerFlag: false,
+  colorPickerFlag: false,
+  alphaPickerFlag: false,
+  r: 250,
+  g: 0,
+  b: 0,
+  a: 1
 };
-
-panel.r = 250;
-panel.g = 0;
-panel.b = 0;
-panel.a = 0;
 
 // UZYSKANIE WSPOLRZEDNYCH I HUE Z WPISANYCH RGB
 
@@ -46,11 +47,12 @@ function buildApp() {
   elm.aWrapper = createElm("div", elm.rgbaPanel, "a-wrapper");
 
   elm.hueCanvas = createHueCanvas();
-  elm.huePicker = createElm("div", elm.hueWrapper, "hue-picker picker");
+  elm.huePicker = panel.elm.hue.picker = createElm("div", elm.hueWrapper, "hue-picker picker");
   elm.colorCanvas = createColorCanvas();
-  elm.colorPicker = createElm("div", elm.colorWrapper, "color-picker picker");
+  elm.colorPicker = panel.elm.color.picker = createElm("div", elm.colorWrapper, "color-picker picker");
+  elm.rgbaValue = createElm("p", elm.colorWrapper, "rgba-value");
   elm.alphaCanvas = createAlphaCanvas();
-  elm.alphaPicker = createElm("div", elm.alphaWrapper, "alpha-picker picker");
+  elm.alphaPicker = panel.elm.alpha.picker = createElm("div", elm.alphaWrapper, "alpha-picker picker");
   elm.hexInput = createElm("input", elm.hexWrapper, "hex-input");
   elm.rInput = createElm("input", elm.rWrapper, "r-input");
   elm.gInput = createElm("input", elm.gWrapper, "g-input");
@@ -71,7 +73,7 @@ panel.yPer = panel.max/255;
 //panel.color = panel.saturation * (1 - panel.yPer)
 
 
-panel.hue = [];
+panel.hue = [];p
   if (panel.r == panel.max) {
     panel.hue[0] = 255;
     if (panel.g == panel.min) {
@@ -167,39 +169,46 @@ function getElm(elm, type) {
   a.elm = elm;
   a.rect = elm.getBoundingClientRect();
   a.picker = elm.querySelector(".picker");
+  a.pickerRect = a.picker.getBoundingClientRect();
   a.x = panel.mouse.x - a.rect.left + a.elm.scrollLeft;
   a.y = panel.mouse.y - a.rect.top + a.elm.scrollTop;
+  if(a.x < 0) { a.x = a.elm.scrollLeft };
+  if(a.x > a.rect.width) { a.x = a.elm.scrollLeft + a.rect.width };
+  if(a.y > a.rect.height) { a.y = a.elm.scrollTop + a.rect.height };
+  if(a.y < 0) { a.y = a.elm.scrollTop};
   a.width = a.rect.width;
   a.height = a.rect.height;
   a.xPer = a.x / a.width;
   a.yPer = a.y / a.height;
-  console.log(panel.elm);
 }
 
 function updatePanelHue(parent) {
   panel.hue[0] = Math.floor(hueGradient(parent, panel, panel.elm.hue.x, panel.elm.hue.y, 0));
   panel.hue[1] = Math.floor(hueGradient(parent, panel, panel.elm.hue.x, panel.elm.hue.y, 2));
   panel.hue[2] = Math.floor(hueGradient(parent, panel, panel.elm.hue.x, panel.elm.hue.y, 4));
-  console.log(panel.hue)
-  console.log(panel.r, panel.g, panel.b)
 }
 
 function updatePanelColor(parent) {
   panel.r = colorGradient(panel.hue[0], parent, panel.elm.color.x, panel.elm.color.y);
   panel.g = colorGradient(panel.hue[1], parent, panel.elm.color.x, panel.elm.color.y);
   panel.b = colorGradient(panel.hue[2], parent, panel.elm.color.x, panel.elm.color.y);
-  console.log(panel.r, panel.g, panel.b, "hello")
+  panel.a = panel.alphaPickerFlag ? panel.elm.alpha.y/panel.elements.alphaCanvas.height : 1;
 }
 
 function getMouse(e) {
   panel.mouse.x = e.clientX;
   panel.mouse.y = e.clientY;
-  console.log(panel.mouse);
 }
 
 function updatePicker(type) {
-  panel.elm[type].picker.style.top = panel.elm[type].y;
-  panel.elm[type].picker.style.left = panel.elm[type].x;
+  if(type == "hue") {
+    panel.elm[type].picker.style.left = panel.elm[type].x - (panel.elm[type].pickerRect.width/2) + "px";
+  } else if(type == "alpha") {
+    panel.elm[type].picker.style.top = panel.elm[type].y - (panel.elm[type].pickerRect.height/2) + "px";
+  } else {
+    panel.elm[type].picker.style.left = panel.elm[type].x - (panel.elm[type].pickerRect.width/2) + "px";
+    panel.elm[type].picker.style.top = panel.elm[type].y - (panel.elm[type].pickerRect.height/2) + "px";
+  }
 }
 
 // MOUSEDOWN HUE WRAPPER
@@ -253,18 +262,16 @@ function drawColorCanvas(parent, elm, panel) {
   for(var i=0; i<imgdatalength/4; i++) {
     var x = i % parent.width;
     var y = Math.floor(i / parent.width);
-    imgdata.data[4*i] = colorGradient(panel.r, parent, x, y);
-    imgdata.data[4*i+1] = colorGradient(panel.g, parent, x, y);
-    imgdata.data[4*i+2] = colorGradient(panel.b, parent, x, y);
+    imgdata.data[4*i] = colorGradient(panel.hue[0], parent, x, y);
+    imgdata.data[4*i+1] = colorGradient(panel.hue[1], parent, x, y);
+    imgdata.data[4*i+2] = colorGradient(panel.hue[2], parent, x, y);
     imgdata.data[4*i+3] = 255;
   }
   ctx.putImageData(imgdata, 0, 0);
-  panel.elements.headerContainer.style.background = `rgb(${panel.r}, ${panel.g}, ${panel.b})`
-  panel.elements.footerContainer.style.background = `rgb(${panel.r}, ${panel.g}, ${panel.b})`
 }
 
 function colorGradient(color, parent, x, y) {
-  return Math.floor((255-(255-color)*(x/parent.width))*((parent.height-y)/parent.height));
+  return Math.ceil((255-(255-color)*(x/parent.width))*((parent.height-y)/parent.height));
 }
 
 //MOUSEDOWN APLHA WRAPPER
@@ -284,17 +291,85 @@ function drawAlphaCanvas(parent, elm, panel) {
   ctx.putImageData(imgdata, 0, 0);
 }
 
-panel.elements.hueWrapper.addEventListener("mousedown", function(e) {
-  getMouse(e);
-  getElm(this, "hue");
-  updatePanelHue(panel.elements.hueCanvas);
-  updatePanelColor(panel.elements.colorCanvas);
-  updatePicker("hue");
-  drawColorCanvas(panel.elements.colorCanvas, panel.elements.colorCanvas, panel);
-  drawAlphaCanvas(panel.elements.alphaCanvas, panel.elements.alphaCanvas, panel);
-  console.log(this)
+function rgbToHex(r,g,b) {return toHex(r)+toHex(g)+toHex(b)}
+function toHex(n) {
+ n = parseInt(n,10);
+ if (isNaN(n)) return "00";
+ n = Math.max(0,Math.min(n,255));
+ return "0123456789ABCDEF".charAt((n-n%16)/16)
+      + "0123456789ABCDEF".charAt(n%16);
+}
 
+function updateColor() {
+  panel.elements.headerContainer.style.background = `rgba(${panel.r}, ${panel.g}, ${panel.b}, ${panel.a})`
+  panel.elements.footerContainer.style.background = `rgba(${panel.r}, ${panel.g}, ${panel.b}, ${panel.a})`
+  panel.elm.hue.picker.style.backgroundColor = `rgb(${panel.hue[0]}, ${panel.hue[1]}, ${panel.hue[2]})`;
+  panel.elm.color.picker.style.backgroundColor = `rgb(${panel.r}, ${panel.g}, ${panel.b})`;
+  panel.elm.alpha.picker.style.backgroundColor = `rgba(${panel.r}, ${panel.g}, ${panel.b}, ${panel.a})`;
+  panel.elements.hexInput.value = "#" + rgbToHex(panel.r, panel.g, panel.b);
+  panel.elements.rInput.value = `${panel.r}`;
+  panel.elements.gInput.value = `${panel.g}`;
+  panel.elements.bInput.value = `${panel.b}`;
+  panel.elements.aInput.value = `${(panel.a).toFixed(2)}`;
+  panel.elements.rgbaValue.innerText = `rgba(${panel.r}, ${panel.g}, ${panel.b}, ${(panel.a).toFixed(2)})`
+}
+
+panel.elements.hueWrapper.addEventListener("mousedown", function(e) {
+  panel.huePickerFlag = true;
+  startHueUpdate(e, this);
+  document.addEventListener("mousemove", function(event) {
+    if(panel.huePickerFlag) {
+      startHueUpdate(event, panel.elements.hueWrapper);
+    }
+  });
+  document.addEventListener("mouseup", function() { panel.huePickerFlag = false });
+  function startHueUpdate(e, elem) {
+    getMouse(e);
+    getElm(elem, "hue");
+    updatePanelHue(panel.elements.hueCanvas);
+    updatePanelColor(panel.elements.colorCanvas);
+    updatePicker("hue");
+    drawColorCanvas(panel.elements.colorCanvas, panel.elements.colorCanvas, panel);
+    drawAlphaCanvas(panel.elements.alphaCanvas, panel.elements.alphaCanvas, panel);
+    updateColor()
+  }
 })
 
+panel.elements.colorWrapper.addEventListener("mousedown", function(e) {
+  panel.colorPickerFlag = true;
+  startColorUpdate(e, this);
+  document.addEventListener("mousemove", function(event) {
+    if(panel.colorPickerFlag) {
+      startColorUpdate(event, panel.elements.colorWrapper);
+    }
+  })
+  document.addEventListener("mouseup", function() { panel.colorPickerFlag = false });
+  function startColorUpdate(e, elem) {
+    getMouse(e);
+    getElm(elem, "color");
+    updatePanelColor(panel.elements.colorCanvas);
+    updatePicker("color");
+    drawAlphaCanvas(panel.elements.alphaCanvas, panel.elements.alphaCanvas, panel);
+    updateColor()
+  }
+})
 
-var picker = createElm("div", wrapper, "picker");
+panel.elements.alphaWrapper.addEventListener("mousedown", function(e) {
+  panel.alphaPickerFlag = true;
+  startAlphaUpdate(e, this);
+  document.addEventListener("mousemove", function(event) {
+    if(panel.alphaPickerFlag) {
+      startAlphaUpdate(event, panel.elements.alphaWrapper);
+    }
+  });
+  document.addEventListener("mouseup", function() { panel.alphaPickerFlag = false });
+  function startAlphaUpdate(e, elm) {
+    getMouse(e);
+    getElm(elm, "alpha");
+    updatePanelColor(panel.elements.colorCanvas);
+    updatePicker("alpha");
+    updateColor();
+  }
+})
+
+updateColor();
